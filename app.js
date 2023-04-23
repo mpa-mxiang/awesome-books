f256337f232052fbad5dcb61e20e376db41191fb
Jiachun Xiang <max997123@gmail.com>
2023-04-23 19:45:55 -0400

    add local storage

--- app.js
+++ app.js
@@ -1,61 +1,56 @@
-// Define a global array to store the book collection
-let bookCollection = [];
+// Load saved book collection from local storage
+let bookCollection = localStorage.getItem("bookCollection");
+if (bookCollection) {
+  bookCollection = JSON.parse(bookCollection);
+} else {
+  bookCollection = [];
+}
+
+// Function to display all books saved in the collection
+function displayBooks() {
+  const bookList = document.getElementById("bookList");
+  bookList.innerHTML = "";
+  bookCollection.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.innerHTML = `<h2>${book.title}</h2><p>${book.author}</p><button class="removeButton" data-title="${book.title}">Remove</button>`;
+    bookList.appendChild(bookDiv);
+  });
+}
 
 // Function to add a new book to the collection
 function addBook() {
-    // Get the input values
-    const title = document.getElementById("title").value;
-    const author = document.getElementById("author").value;
-
-    // Create a new book object and add it to the collection
-    const newBook = { title, author };
-    bookCollection.push(newBook);
-
-    // Clear the input fields
-    document.getElementById("title").value = "";
-    document.getElementById("author").value = "";
-
-    // Refresh the book list
-    displayBooks();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const book = { title, author };
+  bookCollection.push(book);
+  localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
+  displayBooks();
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
 }
 
 // Function to remove a book from the collection
-function removeBook(index) {
-    // Remove the book at the given index from the collection
-    bookCollection.splice(index, 1);
-
-    // Refresh the book list
-    displayBooks();
+function removeBook(title) {
+  bookCollection = bookCollection.filter((book) => book.title !== title);
+  localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
+  displayBooks();
 }
 
-// Function to display all books in the collection
-function displayBooks() {
-    const bookList = document.getElementById("book-list");
+// Add event listener for form submission
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addBook();
+});
 
-    // Clear the book list
-    bookList.innerHTML = "";
+// Add event listener for remove button clicks (using event delegation)
+const bookList = document.getElementById("bookList");
+bookList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("removeButton")) {
+    const title = e.target.dataset.title;
+    removeBook(title);
+  }
+});
 
-    // Add each book to the book list
-    for (let i = 0; i < bookCollection.length; i++) {
-        const book = bookCollection[i];
-
-        const li = document.createElement("li");
-        const title = document.createElement("span");
-        const author = document.createElement("span");
-        const removeBtn = document.createElement("button");
-
-        title.textContent = book.title;
-        author.textContent = ` by ${book.author}`;
-        removeBtn.textContent = "Remove";
-        removeBtn.addEventListener("click", () => removeBook(i));
-
-        li.appendChild(title);
-        li.appendChild(author);
-        li.appendChild(removeBtn);
-        bookList.appendChild(li);
-    }
-}
-
-// Add event listener to the Add button
-document.getElementById("add-btn").addEventListener("click", addBook);
-
+// Display the initial book collection
+displayBooks();
