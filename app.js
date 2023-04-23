e9b617e206d690401b7310571b51e46dcb54ac20
Jiachun Xiang <max997123@gmail.com>
2023-04-23 12:55:16 -0400

    add local storage feature

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 
+const typedTitle = document.querySelector('.title');
+const authorInput = document.querySelector('.author');
+const messageInput = document.querySelector('#comments');
+const books = document.getElementById('contact-form');
+const bookData = JSON.parse(localStorage.getItem('bookData')) || {};
+const savedData = JSON.parse(localStorage.getItem('bookData'));
+if (bookData.title) {
+  typedTitle.value = bookData.title;
+}
+if (bookData.author) {
+  authorInput.value = bookData.author;
+}
+books.addEventListener('input', (event) => {
+  bookData[event.target.name] = event.target.value;
+  localStorage.setItem('formData', JSON.stringify(bookData));
+});
+if (savedData) {
+  authorInput.value = savedData.author;
+  typedName.value = savedData.title;
+}
+// error is here
+book.addEventListener('submit', () => {
+  localStorage.clear();
+});
