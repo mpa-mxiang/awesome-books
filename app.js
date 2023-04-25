fbe1e40a51e070aac9ae6902c20c787204c6e58a
Jiachun Xiang <max997123@gmail.com>
2023-04-25 11:18:22 -0400

    add local storage

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ class Book {
     this.author = author;
   }
 }
+const typedTitle = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const books = document.getElementById('book-form');
+const bookData = JSON.parse(localStorage.getItem('bookData')) || {};
+const savedData = JSON.parse(localStorage.getItem('bookData'));
 
+if (bookData.title) {
+  typedTitle.value = bookData.title;
+}
+if (bookData.author) {
+  authorInput.value = bookData.author;
+}
+books.addEventListener('input', (event) => {
+  bookData[event.target.name] = event.target.value;
+  localStorage.setItem('bookData', JSON.stringify(bookData));
+});
+if (savedData) {
+  authorInput.value = savedData.author;
+  typedTitle.value = savedData.title;
+}
+
+books.addEventListener('submit', () => {
+  localStorage.clear();
+});
 class BookCollection {
   constructor() {
     let bookCollection = localStorage.getItem("bookCollection");
