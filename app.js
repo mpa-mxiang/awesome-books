6460ddaef1f3d647b5f5da3e6124e5b5bbc8510c
Jiachun Xiang <max997123@gmail.com>
2023-04-25 10:50:07 -0400

    merge from temp branch

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
-class Display {
-  books = [];
 
-<<<<<<< HEAD
-  bookListContainer = document.getElementById('bookList');
-
-  constructor() {
-    if (localStorage.getItem('books')) {
-      this.books = JSON.parse(localStorage.getItem('books')).map((book) => new Book(book.title, book.author, book.id));
-    }
-  }
-
-  addBook() {
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const book = { title, author };
-    bookCollection.push(book);
-    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-    displayBooks();
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-  }
-
-  render() {
-
-  }
-
-  saveBooks() {
-    localStorage.setItem('books', JSON.stringify(this.books));
-  }
-}
-
-// Load saved book collection from local storage
-let bookCollection = localStorage.getItem('bookCollection');
-=======
 // Load saved book collection from local storage
 let bookCollection = localStorage.getItem("bookCollection");
->>>>>>> origin/temp
 if (bookCollection) {
   bookCollection = JSON.parse(bookCollection);
 } else {
@@ -47,10 +12,7 @@ const authorInput = document.querySelector('#author');
 const books = document.getElementById('book-form');
 const bookData = JSON.parse(localStorage.getItem('bookData')) || {};
 const savedData = JSON.parse(localStorage.getItem('bookData'));
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/temp
 if (bookData.title) {
   typedTitle.value = bookData.title;
 }
@@ -59,25 +21,6 @@ if (bookData.author) {
 }
 books.addEventListener('input', (event) => {
   bookData[event.target.name] = event.target.value;
-<<<<<<< HEAD
-  localStorage.setItem('formData', JSON.stringify(bookData));
-});
-if (savedData) {
-  authorInput.value = savedData.author;
-  typedName.value = savedData.title;
-}
-// error is here
-book.addEventListener('submit', () => {
-  localStorage.clear();
-});
-// Function to display all books saved in the collection
-function displayBooks() {
-  const bookList = document.getElementById('bookList');
-  bookList.innerHTML = '';
-  bookCollection.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.innerHTML = `<h2>${book.title}</h2><p>${book.author}</p><button class="removeButton" data-title="${book.title}">Remove</button>`;
-=======
   localStorage.setItem('bookData', JSON.stringify(bookData));
 });
 if (savedData) {
@@ -96,23 +39,12 @@ function displayBooks() {
   bookCollection.forEach((book) => {
     const bookDiv = document.createElement("div");
     bookDiv.innerHTML = `<h2>${book.title}</h2><p>${book.author}</p><button class="removeButton" data-title="${book.title}">Remove</button><hr />`;
->>>>>>> origin/temp
     bookList.appendChild(bookDiv);
   });
 }
 
 // Function to add a new book to the collection
 function addBook() {
-<<<<<<< HEAD
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const book = { title, author };
-  bookCollection.push(book);
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-  displayBooks();
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-=======
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const book = { title, author };
@@ -121,44 +53,27 @@ function addBook() {
   displayBooks();
   document.getElementById("title").value = "";
   document.getElementById("author").value = "";
->>>>>>> origin/temp
 }
 
 // Function to remove a book from the collection
 function removeBook(title) {
   bookCollection = bookCollection.filter((book) => book.title !== title);
-<<<<<<< HEAD
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-=======
   localStorage.setItem("bookCollection", JSON.stringify(bookCollection));
->>>>>>> origin/temp
   displayBooks();
 }
 
 // Add event listener for form submission
-<<<<<<< HEAD
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-=======
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
->>>>>>> origin/temp
   e.preventDefault();
   addBook();
 });
 
 // Add event listener for remove button clicks (using event delegation)
-<<<<<<< HEAD
-const bookList = document.getElementById('bookList');
-bookList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('removeButton')) {
-    const { title } = e.target.dataset;
-=======
 const bookList = document.getElementById("bookList");
 bookList.addEventListener("click", (e) => {
   if (e.target.classList.contains("removeButton")) {
     const title = e.target.dataset.title;
->>>>>>> origin/temp
     removeBook(title);
   }
 });
