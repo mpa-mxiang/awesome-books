1a7d113aacfc85f909f714c6dc8f1a24f9c99da3
Jiachun Xiang <max997123@gmail.com>
2023-04-26 16:16:16 -0400

    fix navs

--- app.js
+++ app.js
@@ -1,81 +1,58 @@
-// Load saved book collection from local storage
-let bookCollection = localStorage.getItem('bookCollection');
-if (bookCollection) {
-  bookCollection = JSON.parse(bookCollection);
-} else {
-  bookCollection = [];
+class BookCollection {
+  constructor() {
+    this.bookList = JSON.parse(localStorage.getItem('bookCollection')) || [];
+  }
+
+  // Function to display all books saved in the collection
+  displayBooks() {
+    const bookListElement = document.getElementById('bookList');
+    bookListElement.innerHTML = '';
+    this.bookList.forEach((book) => {
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('viewBook');
+      bookDiv.innerHTML = `<span>${book.title} by ${book.author}</span><button class="removeButton" data-title="${book.title}">Remove</button>`;
+      bookListElement.appendChild(bookDiv);
+    });
+  }
+
+  // Function to add a new book to the collection
+  addBook(title, author) {
+    const book = { title, author };
+    this.bookList.push(book);
+    localStorage.setItem('bookCollection', JSON.stringify(this.bookList));
+    this.displayBooks();
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+  }
+
+  // Function to remove a book from the collection
+  removeBook(title) {
+    this.bookList = this.bookList.filter((book) => book.title !== title);
+    localStorage.setItem('bookCollection', JSON.stringify(this.bookList));
+    this.displayBooks();
+  }
 }
 
-const typedTitle = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const books = document.getElementById('book-form');
-const bookData = JSON.parse(localStorage.getItem('bookData')) || {};
-const savedData = JSON.parse(localStorage.getItem('bookData'));
-
-if (bookData.title) {
-  typedTitle.value = bookData.title;
-}
-if (bookData.author) {
-  authorInput.value = bookData.author;
-}
-books.addEventListener('input', (event) => {
-  bookData[event.target.name] = event.target.value;
-  localStorage.setItem('bookData', JSON.stringify(bookData));
-});
-if (savedData) {
-  authorInput.value = savedData.author;
-  typedTitle.value = savedData.title;
-}
-
-books.addEventListener('submit', () => {
-  localStorage.clear();
-});
-
-// Function to display all books saved in the collection
-function displayBooks() {
-  const bookList = document.getElementById('bookList');
-  bookList.innerHTML = '';
-  bookCollection.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.innerHTML = `<h2>${book.title}</h2><p>${book.author}</p><button class="removeButton" data-title="${book.title}">Remove</button><hr />`;
-    bookList.appendChild(bookDiv);
-  });
-}
-
-// Function to add a new book to the collection
-function addBook() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const book = { title, author };
-  bookCollection.push(book);
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-  displayBooks();
-  document.getElementById('title').value = '';
-  document.getElementById('author').value = '';
-}
-
-// Function to remove a book from the collection
-function removeBook(title) {
-  bookCollection = bookCollection.filter((book) => book.title !== title);
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-  displayBooks();
-}
+// Create a new instance of the BookCollection class
+const myBookCollection = new BookCollection();
 
 // Add event listener for form submission
-const form = document.querySelector('form');
+const form = document.querySelector('#book-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  addBook();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  myBookCollection.addBook(title, author);
 });
 
 // Add event listener for remove button clicks (using event delegation)
-const bookList = document.getElementById('bookList');
-bookList.addEventListener('click', (e) => {
+const bookListElement = document.getElementById('bookList');
+bookListElement.addEventListener('click', (e) => {
   if (e.target.classList.contains('removeButton')) {
     const { title } = e.target.dataset;
-    removeBook(title);
+    myBookCollection.removeBook(title);
   }
 });
 
 // Display the initial book collection
-displayBooks();
+myBookCollection.displayBooks();
